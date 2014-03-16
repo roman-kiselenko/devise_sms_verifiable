5079b887a4f6633ccccac9d68de2f7f101f2e953
fishbullet <shindu666@gmail.com>
2014-03-16 15:17:46 +0400

    fix missing constant

--- lib/devise_sms_verifiable/engine.rb
+++ lib/devise_sms_verifiable/engine.rb
@@ -1,3 +1,6 @@
+require 'devise_sms_verifiable/controllers/internal_helpers'
+require 'devise_sms_verifiable/controllers/url_helpers'
+
 module DeviseSmsVerifiable
   class Engine < ::Rails::Engine
     isolate_namespace DeviseSmsVerifiable
