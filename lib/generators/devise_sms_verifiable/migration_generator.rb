9bb84ce6a99ccf1389bf4accdfdde49920188403
fishbullet <shindu666@gmail.com>
2014-03-22 16:06:17 +0400

    generator

---
+++ lib/generators/devise_sms_verifiable/migration_generator.rb
@@ -0,0 +1,54 @@
+require 'rails/generators/base'
+
+module DeviseSmsVerifiable
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path("../templates", __FILE__)
+
+    def add_migrations
+      default_migration_path = ""
+      #if File.exist?(devise_initializer_path)
+        #old_content = File.read(devise_initializer_path)
+
+        #if old_content.match(Regexp.new(/^\s# ==> Configuration for :sms_verifiable\n/))
+          #false
+        #else
+          #inject_into_file(devise_initializer_path, before: "# ==> Configuration for :database_authenticatable\n") do
+#<<-CONTENT
+## ==> Configuration for :sms_verifiable
+  ## Field in model for confirm phone(must be boolean)
+  ##config.phone_confirmation_field = :phone_confirm
+  ## Method for check sms confirmation. Define in ApplicationController
+  ##config.sms_confirmation_method = :sms_confirmation?
+  ## Method generate secret and write to model. Define in ApplicationController
+  ##config.sms_secret_method = :sms_secret
+  ## Attribut on model return rigth answer
+  ##config.sms_answer_field = :sms_answer
+  ## Field in model which phone number
+  ##config.phone_field = :phone
+  ## Field in model for confirm phone(must be boolean)
+  ##config.phone_confirmation_field = :phone_confirm
+  ## Path after successful confirmation
+  ##config.successful_path = '/'
+  ## SMS provider module(have default for STREAM-TELECOM)
+  ##config.sms_provider = DeviseSmsVerifiable::Provider
+  ## Send sms silent for test
+  ##config.provider_silent = true
+  ## SMS provider login
+  ##config.provider_login = ""
+  ## SMS provider password
+  ##config.provider_password = ""
+  ## SMS provider from
+  ##config.provider_from = ""
+  #\n
+#CONTENT
+          #end
+        #end
+      #end
+    end
+
+    #def show_readme
+      #readme "README"
+    #end
+  end
+end
+
